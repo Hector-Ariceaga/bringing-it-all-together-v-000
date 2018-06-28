d2c86a7cf9aecd337f65189fc16c083349be42f7
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-06-28 16:45:58 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -52,8 +52,9 @@ class Dog
   end 
   
   def self.new_from_db(row)
-    dog = Dog.new(row[0], row[1], row[2])
     binding.pry
+    dog = Dog.new(row[0], row[1], row[2])
+    
   end
   
   def self.find_by_name
